f97b781bd1fd667dfec60d2ae07d7c7665ee8637
luli <luli@sonicwall.com>
2019-02-15 14:13:51 +0800

    Initial commit

---
+++ RF-Capacity/Capacity/__init__.py
@@ -0,0 +1,8 @@
+from bpsRest import bpsRest
+from version import VERSION
+
+__version__='0.1'
+
+
+class  breakingpoint(bpsRest):
+	ROBOT_LIBRARY_SCOPE ='GLOBAL'
---
+++ RF-Capacity/Sonicwall/__init__.py
@@ -0,0 +1,8 @@
+from bptest import bptest
+from version import VERSION
+
+__version__='0.1'
+
+
+class Sonicwall(bptest):
+	 ROBOT_LIBRARY_SCOPE ='GLOBAL'
